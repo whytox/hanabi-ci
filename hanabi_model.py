546e6597901e1d0cf7244682e30b6ad8d8d15736
Whytox <vito.ruggirello@gmail.com>
2022-02-03 20:08:01 +0100

    added methods to generate set of cards with specific properties

--- hanabi_model.py
+++ hanabi_model.py
@@ -2,10 +2,8 @@ from abc import ABC
 import GameData
 from game import Player, Card
 from itertools import product
-
+from collections import Counter
 import logging
-
-from hanabi_view import player_hand
 
 logging.basicConfig(format="%(message)s", level=logging.DEBUG)
 
@@ -91,9 +89,14 @@ class Discard(HanabiAction):
 
 
 class Inference:
-    def __init__(self, n_cards, visible_cards: set):
+    def __init__(self, n_cards, other_players_card: set):
+
         self.n_cards = n_cards
-        self.my_hand = [UnknownCard(visible_cards) for _ in range(self.n_cards)]
+        self.visible_cards = other_players_card
+        self.my_hand = [UnknownCard(self.visible_cards) for _ in range(self.n_cards)]
+        # print(f"{self.my_hand}")
+        # TODO: keep track of players that didn't follow a certain convention
+        self.not_trusted_players = set()
         return
 
     def add_hint(self, hint: Hint):
@@ -107,11 +110,31 @@ class Inference:
                 c.add_negative_knowledge(hint)
         return
 
+    def add_new_unknown_card(self, hanabi_action: HanabiAction):
+        """Create a new unknown card after a previous one has
+        been played or discarded."""
+        card_index = hanabi_action.card_index
+        # print(card_index)
+        # print(f"\tBefore: {self.my_hand[card_index]}")
+        # the new card is placed at the end of the player hand
+        # so: remove the played card and append an unknown card at the end
+        self.my_hand = self.my_hand[:card_index] + self.my_hand[card_index + 1 :]
+        self.my_hand.append(UnknownCard(self.visible_cards))
+        print(len(self.my_hand))
+        # print(f"\tAfter: {self.my_hand[card_index]}")
+
+    def add_visible_card(self, card: Card):
+        """Add a new visible card to the set of visible cards."""
+        self.visible_cards.add(card)
+        for u in self.my_hand:
+            u.remove_possible(card)
+        return
+
 
 class UnknownCard:
     """Represent the agent unknown card."""
 
-    DECK_DISTR = [(1, 3), (2, 2), (3, 2), (4, 2), (5, 1)]
+    DECK_DISTR = {1: 3, 2: 2, 3: 2, 4: 2, 5: 1}
     RED = "red"
     YELLOW = "yellow"
     GREEN = "green"
@@ -120,22 +143,15 @@ class UnknownCard:
 
     COLORS = [RED, YELLOW, GREEN, BLUE, WHITE]
 
-    def __init__(self, other_player_cards=None, table=None, discard_pile=None):
-        self.received_hints = list()
-        if other_player_cards is None:
-            other_player_cards = set()
-        if table is None:
-            table = set()
-        if discard_pile is None:
-            discard_pile = set()
-        if other_player_cards is None:
-            other_player_cards = set()
-        self.possible_cards = (
-            UnknownCard.all_possible_cards()
-            .difference(other_player_cards)
-            .difference(table)
-            .difference(discard_pile)
+    def __init__(self, not_possible_cards=None):
+        if not_possible_cards is None:
+            not_possible_cards = set()
+
+        self.possible_cards = UnknownCard.all_possible_cards().difference(
+            not_possible_cards
         )
+        if not self.possible_cards:
+            raise ValueError(f"Empty possible cards, but {not_possible_cards} given")
         self.received_hints = list()
         self.not_received_hints = list()
 
@@ -150,6 +166,7 @@ class UnknownCard:
                 not_possible_cards.add(c)
         # remove the set of non possible cards from the set of possible cards
         self.possible_cards = self.possible_cards.difference(not_possible_cards)
+        # print("after hint registration possible cards are:", self.possible_cards)
         return
 
     def add_negative_knowledge(self, hint: Hint):
@@ -160,21 +177,59 @@ class UnknownCard:
             if hint.covers(c):
                 not_possible_cards.add(c)
         self.possible_cards = self.possible_cards.difference(not_possible_cards)
+        # print("after NON hint registration possible cards are:", self.possible_cards)
+        return
+
+    def remove_possible(self, card: Card):
+        if card in self.possible_cards:
+            self.possible_cards.remove(card)
         return
 
     @staticmethod
     def all_possible_cards():
         card_set = set()
         card_id = 0
-        for num, copies in UnknownCard.DECK_DISTR:
+        for num, copies in UnknownCard.DECK_DISTR.items():
             for _, color in product(range(copies), UnknownCard.COLORS):
                 # use server side Card class
                 card_set.add(Card(card_id, num, color))
                 card_id += 1
+        print(card_set)
         return card_set
 
+    NUM_BASE_IDS = {1: 0, 2: 9, 3: 24, 4: 34, 5: 45}
+
+    @staticmethod
+    def possible_cards_with_number(number: int):
+        cards = set()
+        id = UnknownCard.NUM_BASE_IDS[number]
+        for _, color in product(
+            range(UnknownCard.DECK_DISTR[number]), UnknownCard.COLORS
+        ):
+            cards.add(Card(id, number, color))
+            id += 1
+        return cards
+
+    COLOR_BASE_IDS = {RED: 0, YELLOW: 1, GREEN: 2, BLUE: 3, WHITE: 4}
+
+    @staticmethod
+    def possible_cards_with_color(color: str):
+        cards = set()
+        id = UnknownCard.COLOR_BASE_IDS[color]
+        for number in range(1, 6):
+            for _ in range(UnknownCard.DECK_DISTR[number]):
+                cards.add(Card(id, number, color))
+                id += 5
+        return cards
+
+    @staticmethod
+    def possible_cards_with_info(number: int, color: str):
+        color_set = UnknownCard.possible_cards_with_color(color)
+        number_set = UnknownCard.possible_cards_with_number(number)
+        return number_set & color_set
+
     def __str__(self):
-        return "?"
+        return "?" if len(self.possible_cards) == 50 else str(self.possible_cards)
 
     def __repr__(self) -> str:
         return self.__str__()
@@ -202,14 +257,9 @@ class HanabiState:
 
         visible_cards = self.get_visible_cards()
         self.inference = Inference(self.n_cards, visible_cards)
-        # self.hint_history =
-        # <--------------------------------------
-
-        # self.discard_history = list()
-        # self.play_history = list()
-        # self.error_history = list()
-        # self.hint_history = list()
-        # self.received_hints = list()
+        # track the given hint using a list of list
+        # the first index is the player (in turn order)
+        self.given_hints = [list() for _ in range(len(self.players_list))]
         return
 
     def get_my_turn(self):
@@ -255,17 +305,38 @@ class HanabiState:
 
     def on_play(self, play: Play):
         logging.debug(f"card drawn: {play.card_drawn}")
-        # TODO: self.inference.add_visible_card(play.card_drawn)
-        # self.inference.update_visible_cards()
+
+        if play.sender == self.my_name:
+            print(
+                f"-- adding a new unknown card after a play in position {play.card_index}"
+            )
+            self.inference.add_new_unknown_card(play)
+        else:
+            self.inference.add_visible_card(play.card_drawn)
         return
 
     def on_discard(self, discard: Discard):
-        # TODO: update state on discard's result
-        # self.inference.update_visible_cards()
-        logging.debug(
-            f"{discard.sender} discarded the card {discard.card_discarded} at index {discard.card_index} and drawn {discard.card_drawn}"
-        )
+        if discard.sender == self.my_name:
+            self.inference.add_new_unknown_card(discard)
+        else:
+            self.inference.add_visible_card(discard.card_drawn)
         return
+
+    def get_cards_to_be_played(self):
+        """Return a tuple (number, color) for each pile
+        non completely fullfilled with cards,
+        which represents the next card to be played."""
+        to_be_played = list()
+        for color, cards in self.table_cards.items():
+            # next number
+            if not cards:
+                next_number = 1
+            elif len(cards) == 5:
+                continue  # we don't want to play 6s
+            else:
+                next_number = max(cards, key=lambda c: c.value).value + 1
+            to_be_played.append((next_number, color))
+        return to_be_played
 
     def __str__(self):
         note_tokens = f"{self.used_note_tokens}/8"
@@ -273,20 +344,20 @@ class HanabiState:
         player_data = [f"{p.name}: {p.hand}" for p in self.players_list]
         return "\n".join([*player_data, note_tokens, storm_tokens])
 
-    # def valid_hints(self, player_name: str, hint_type=None) -> list:
-    #     """Return the valid hints that can be given to player `player_name`
-    #     as a tuple (type, value, n_cards).
-    #     TODO: add remove_known param to filter out already known hints."""
-    #     hints_for_player = list()
-    #     player = self.get_player(player_name)
-    #     for i, c in enumerate(player.hand):
-    #         card_info = [c.value, c.color]  # no card index??
-    #         hints_for_player.extend(card_info)
-    #     if hint_type == "color":
-    #         hint_filt = lambda info: type(info) == str  # card color hint
-    #     elif hint_type == "value":
-    #         hint_filt = lambda info: type(info) == int  # card value hint
-    #     else:
-    #         return Counter(hints_for_player)
-    #     hints_for_player = filter(hint_filt, hints_for_player)
-    #     return Counter(hints_for_player)
+    def valid_hints(self, player_name: str, hint_type=None) -> list:
+        """Return the valid hints that can be given to player `player_name`
+        as a tuple (type, value, n_cards).
+        TODO: add remove_known param to filter out already known hints."""
+        hints_for_player = list()
+        player = self.get_player(player_name)
+        for i, c in enumerate(player.hand):
+            card_info = [c.value, c.color]  # no card index??
+            hints_for_player.extend(card_info)
+        if hint_type == "color":
+            hint_filt = lambda info: type(info) == str  # card color hint
+        elif hint_type == "value":
+            hint_filt = lambda info: type(info) == int  # card value hint
+        else:
+            return Counter(hints_for_player)
+        hints_for_player = filter(hint_filt, hints_for_player)
+        return Counter(hints_for_player)
