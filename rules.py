6d47431c3c51b674ce7f1f3d36a1641ee1becde4
Whytox <vito.ruggirello@gmail.com>
2022-02-03 23:17:22 +0100

    hint playable implementation

--- rules.py
+++ rules.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from game import Game
-from hanabi_model import HanabiState, HanabiAction, Hint, Play, Discard
+from hanabi_model import HanabiState, HanabiAction, Hint, Play, Discard, UnknownCard
 from itertools import product
 import random
 
@@ -75,9 +75,40 @@ class DiscardSafeCard(Rule):
         pass
 
 
-class HintPlayableCards(Rule):
+class HintPlayableCard(Rule):
     def match(state: HanabiState) -> HanabiAction:
-        pass
+        if state.used_note_tokens == 8:
+            return None
+
+        playable_cards = state.get_valid_playable_cards()
+        # order the player starting from the one following me
+        players = (
+            state.players_list[state.my_turn + 1 :]
+            + state.players_list[: state.my_turn]
+        )
+
+        for player in players:
+            player_cards = set(player.hand)
+            # if there are some playable cards in player's hand...
+            hintable_cards = player_cards & playable_cards
+            print(state.my_name.upper(), hintable_cards, sep="\t")
+            for card in hintable_cards:
+                # TODO: only clue unclued cards
+                same_color_cards = UnknownCard.possible_cards_with_color(card.color)
+                same_value_cards = UnknownCard.possible_cards_with_number(card.value)
+                # and the player doesn't have other non-hintable
+                # cards with the same color...
+                if not (player_cards - hintable_cards) & same_color_cards:
+                    return Hint(
+                        state.my_name, player.name, Hint.HINT_TYPE_COL, card.color
+                    )
+                # or if the player doesn't have other non-hintable
+                # cards with the same number...
+                if not (player_cards - hintable_cards) & same_value_cards:
+                    return Hint(
+                        state.my_name, player.name, Hint.HINT_TYPE_VAL, card.value
+                    )
+        return None
 
 
 class FinesseCard(Rule):
@@ -86,25 +117,5 @@ class FinesseCard(Rule):
 
 
 class MustDiscard(Rule):
-    def match(state: HanabiState) -> HanabiAction:
-        pass
-
-
-class HintPlayableCard(Rule):
-    def match(state: HanabiState) -> HanabiAction:
-        pass
-
-
-class DiscardOldCard(Rule):
-    def match(state: HanabiState) -> HanabiAction:
-        pass
-
-
-class HintSafeDiscard(Rule):
-    def match(state: HanabiState) -> HanabiAction:
-        pass
-
-
-class PlayImplicitCard(Rule):
     def match(state: HanabiState) -> HanabiAction:
         pass
